Fix scaling, fix initial action, add clipping, simple snr

dataset.py
@@ -27,13 +27,15 @@ class OasisDataset(Dataset):
         data_dir,
         image_size,
         max_seq_len,
-        max_datapoints=None
+        max_datapoints=None,
+        load_actions=True
     ):
         assert max_seq_len > 0
 
         self.data_dir = data_dir
         self.image_size = image_size
         self.max_seq_len = max_seq_len
+        self.load_actions = load_actions
 
         self.datapoints = []
         unique_ids = glob.glob(os.path.join(data_dir, "*.mp4"))
@@ -49,8 +51,8 @@ class OasisDataset(Dataset):
             except:
                 continue
 
-            if frame_count < max_seq_len:
-                continue
+            #if frame_count < max_seq_len:
+            #    continue
 
             self.datapoints.append({
                 "id": id,
@@ -71,41 +73,47 @@ class OasisDataset(Dataset):
 
         data = self.datapoints[idx]
 
-        seq_len = self.max_seq_len
-        start_frame = random.randint(1, data["frame_count"] - seq_len) - 1
+        seq_len = min(self.max_seq_len, data["frame_count"])
+        start_frame = random.randint(0, data["frame_count"] - seq_len)
 
-        with open(data["json_path"]) as json_file:
-            json_lines = json_file.readlines()
-            json_data = "[" + ",".join(json_lines) + "]"
-            json_data = json.loads(json_data)
+        if self.load_actions:
+            with open(data["json_path"]) as json_file:
+                json_lines = json_file.readlines()
+                json_data = "[" + ",".join(json_lines) + "]"
+                json_data = json.loads(json_data)
+        else:
+            json_data = [0] * seq_len
 
         frames = []
         actions = []
         with av.open(data["video_path"]) as video:
-            """
             # Rough estimate of the desired frame. Could do some more math
             # and decoding to get the exact frame, but it's not that important
             # https://github.com/PyAV-Org/PyAV/discussions/1113
-            stream = video.streams.video[0]
-            seek_sec = start_frame / stream.average_rate
-            seek_ts = int(seek_sec / stream.time_base)
-            video.seek(seek_ts, stream=stream)
-            """
+            if not self.load_actions:
+                stream = video.streams.video[0]
+                seek_sec = start_frame / stream.average_rate
+                seek_ts = int(seek_sec / stream.time_base)
+                video.seek(seek_ts, stream=stream)
 
             frame_iter = video.decode(video=0)
             for i in range(len(json_data)):
                 # TODO: optimize?
-                if i < start_frame:
+                if self.load_actions and i < start_frame:
                     next(frame_iter)
                     continue
 
                 frame = next(frame_iter)
-                action = json_data[i]
-                action, is_null = process_json_action(action)
 
-                # If nothing happened, skip to the next frame
-                if is_null:
-                    continue
+                if self.load_actions:
+                    action = json_data[i]
+                    action, is_null = process_json_action(action)
+
+                    # If nothing happened, skip to the next frame
+                    if is_null:
+                        continue
+
+                    actions.append(action)
 
                 frame = frame.to_ndarray(
                     width=self.image_size[0],
@@ -113,12 +121,12 @@ class OasisDataset(Dataset):
                     format="rgb24"
                 )
                 frames.append([frame])
-                actions.append(action)
 
                 if len(frames) >= seq_len:
                     break
 
         actions = one_hot_actions(actions)
+        actions = torch.cat([torch.zeros_like(actions[:1]), actions], dim=0) # prepend null action
         frames = np.vstack(frames)
         frames = torch.from_numpy(frames)
         frames = rearrange(frames, "t h w c -> t c h w")

generate.py
@@ -31,43 +31,46 @@ def main(args):
     max_noise_level = 1000
     ddim_noise_steps = args.ddim_steps
     noise_range = torch.linspace(-1, max_noise_level - 1, ddim_noise_steps + 1)
-    noise_abs_max = 20
+    noise_clip = 20
     stabilization_level = 15
 
     # get prompt image/video
+    """
     x = load_prompt(
         args.prompt_path,
         video_offset=args.video_offset,
         n_prompt_frames=n_prompt_frames,
         size=(vae.input_width, vae.input_height)
     )
+    x = x * 2 - 1
     # get input action stream
     actions = load_actions(args.actions_path, action_offset=args.video_offset)[:, :total_frames]
-
     """
+
     loader = get_dataloader(
         1,
         data_dir="data",
         image_size=(vae.input_width, vae.input_height),
-        max_seq_len=32,
-        max_datapoints=1
+        max_seq_len=total_frames,
+        #max_datapoints=1
     )
-    x = loader.dataset[0].to(default_device).unsqueeze(0)
-    """
+    x, actions = loader.dataset[0]
+    x = x.unsqueeze(0)
+    actions = actions.unsqueeze(0)
 
     # sampling inputs
     x = x.to(default_device)
     actions = actions.to(default_device)
 
     # vae encoding
-    B = x.shape[0]
+    B, T = x.shape[:2]
     H, W = x.shape[-2:]
-    scaling_factor = 0.07843137255
+    scaling_factor = 0.96
     x = rearrange(x, "b t c h w -> (b t) c h w")
     with torch.no_grad():
         with autocast(default_device, dtype=torch.half):
-            x = vae.encode(x * 2 - 1).mean * scaling_factor
-    x = rearrange(x, "(b t) (h w) c -> b t c h w", t=n_prompt_frames, h=H // vae.patch_size, w=W // vae.patch_size)
+            x = vae.encode(x).mean * scaling_factor
+    x = rearrange(x, "(b t) (h w) c -> b t c h w", t=T, h=vae.seq_h, w=vae.seq_w)
     x = x[:, :n_prompt_frames]
 
     # get alphas
@@ -79,9 +82,34 @@ def main(args):
     # sampling loop
     for i in tqdm(range(n_prompt_frames, total_frames)):
         chunk = torch.randn((B, 1, *x.shape[-3:]), device=default_device)
-        chunk = torch.clamp(chunk, -noise_abs_max, +noise_abs_max)
+        chunk = torch.clamp(chunk, -noise_clip, +noise_clip)
         x = torch.cat([x, chunk], dim=1)
         start_frame = max(0, i + 1 - model.max_frames)
+
+        """
+        def reverse_sample(x, t):
+            alphabar_t = alphas_cumprod[t]
+            alphabar_tm1 = alphas_cumprod[t - 1]
+            beta = rearrange(betas[t], "b t -> b t 1 1 1")
+            alpha = 1 - beta
+            beta_tilde_t = ((1 - alphabar_tm1) / (1 - alphabar_t)) * beta
+            with torch.no_grad():
+                with autocast(default_device, dtype=torch.half):
+                    eps = model(x, t)
+            mu_tilde_t = torch.sqrt(1.0 / alpha) * (x - beta * eps / torch.sqrt(1.0 - alphabar_t))
+            e = torch.randn_like(x)
+            return mu_tilde_t + torch.sqrt(beta_tilde_t) * e
+
+        t_ctx = torch.full((B, i), 0, dtype=torch.long, device=default_device)
+        for ts in reversed(range(max_noise_level)):
+            t = torch.full((B, 1), ts, dtype=torch.long, device=default_device)
+            t = torch.cat([t_ctx, t], dim=1)
+
+            x_pred = reverse_sample(x, t)
+            #print(x_pred[:, 0].max(), x_pred[:, -1:].max())
+            x[:, -1:] = x_pred[:, -1:]
+
+        """
 
         for noise_idx in reversed(range(1, ddim_noise_steps + 1)):
             # set up noise values

sample_vae.py
@@ -27,7 +27,7 @@ def plot_latents(model):
         max_seq_len=1,
     )
 
-    num_images = 1
+    num_images = 3
     fig = plt.figure()
     for i in range(num_images):
         x_in = loader.dataset[i].to(default_device)
@@ -46,6 +46,28 @@ def plot_latents(model):
     plt.show()
 
 
+def compute_scaling_factor(model):
+    all_latents = []
+    loader = get_dataloader(
+        1,
+        data_dir="data",
+        image_size=(model.input_width, model.input_height),
+        max_seq_len=50,
+        load_actions=False
+    )
+    with torch.no_grad():
+        for X, _ in tqdm(loader):
+            X = X.to(default_device)
+            X = rearrange(X, "b t c h w -> (b t) c h w")
+            latents = model.encode(X).sample()
+            all_latents.append(latents.cpu())
+
+    all_latents_tensor = torch.cat(all_latents)
+    std = all_latents_tensor.std().item()
+    normalizer = 1 / std
+    print(f'{normalizer=}')
+
+
 def main(args):
     torch.manual_seed(0)
     torch.cuda.manual_seed(0)
@@ -59,7 +81,8 @@ def main(args):
 
     vae = vae.eval()
 
-    plot_latents(vae)
+    #plot_latents(vae)
+    compute_scaling_factor(vae)
 
 
 if __name__ == "__main__":

train.py
@@ -17,7 +17,7 @@ import os
 
 
 def train_vae(args):
-    _, model = load_models(None, None, parse_img_size(args.default_img_size))
+    _, model = load_models(None, args.vae_ckpt, parse_img_size(args.default_img_size))
     model = model.train()
     model.requires_grad_(True)
 
@@ -35,6 +35,7 @@ def train_vae(args):
         data_dir=args.data_dir,
         image_size=(model.input_width, model.input_height),
         max_seq_len=1,
+        load_actions=False,
         #max_datapoints=1
     )
 
@@ -121,6 +122,7 @@ def train_dit(args):
     alphas_cumprod = torch.cumprod(alphas, dim=0)
     alphas_cumprod = rearrange(alphas_cumprod, "T -> T 1 1 1")
     cum_snr_decay = 0.98
+    noise_clip = 20.0
     snr_clip = 5.0
     snr_arr = alphas_cumprod / (1 - alphas_cumprod)
     clipped_snr_arr = snr_arr.clamp(max=snr_clip)
@@ -150,7 +152,7 @@ def train_dit(args):
             A = A.to(default_device)
 
             B, T = X.shape[:2]
-            scaling_factor = 0.07843137255 # TODO: ?
+            scaling_factor = 0.96
             X = rearrange(X, "b t c h w -> (b t) c h w")
             with torch.no_grad():
                 with autocast(default_device, dtype=torch.half):
@@ -165,15 +167,18 @@ def train_dit(args):
 
             # Calculate the loss
             e = torch.randn_like(X)
+            e = torch.clamp(e, -noise_clip, noise_clip)
             x_t = forward_sample(X, t, e)
             e_pred = model(x_t, t, A)
+            e_pred = torch.clamp(e_pred, -noise_clip, noise_clip)
             loss = torch.nn.functional.mse_loss(e, e_pred, reduction="none")
 
             snr = snr_arr[t]
             clipped_snr = clipped_snr_arr[t]
+
+            """
             normalized_clipped_snr = clipped_snr / snr_clip
             normalized_snr = snr / snr_clip
-
             cum_snr = torch.zeros_like(normalized_snr)
             for frame_idx in range(0, T):
                 if frame_idx == 0:
@@ -185,10 +190,15 @@ def train_dit(args):
             clipped_fused_snr = 1 - (1 - cum_snr * cum_snr_decay) * (1 - normalized_clipped_snr)
             fused_snr = 1 - (1 - cum_snr * cum_snr_decay) * (1 - normalized_snr)
             loss_weight = clipped_fused_snr / fused_snr
+            """
+
+            loss_weight = clipped_snr / snr
             loss_weight = loss_weight.view(*loss_weight.shape, *((1,) * (loss.ndim - 2)))
 
             #loss = (loss * loss_weight).sum() / B
             loss = (loss * loss_weight).mean()
+
+            loss = loss.mean()
 
             loss.backward()
 